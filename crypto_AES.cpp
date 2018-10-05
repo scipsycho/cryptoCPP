d4740dbefd1d6efc71232ce660a45ea71411e0fd
scipsycho <dsingh1664@outlook.com>
2018-10-05 20:51:43 +0530

    generalized functions

--- crypto_AES.cpp
+++ crypto_AES.cpp
@@ -8,9 +8,11 @@ void subBytes_transform(vector< vector< uint8_t > > &state )
 	uint8_t row, col;
 	uint8_t mask = 0x0f;
 	uint8_t s;
-	for(int i=0; i<4; i++)
+	int n = state.size();
+	int m = state[0].size();
+	for(int i=0; i<n; i++)
 	{
-		for(int j=0; j<4; j++)
+		for(int j=0; j<m; j++)
 		{
 			s = (state[i][j]);
 			row = (s & (mask << 4 ) >> 4);
@@ -24,10 +26,12 @@ void shiftRow_left(vector< uint8_t > &row )
 {
 	uint8_t temp;
 	temp   = row[0];
-	row[0] = row[1];
-	row[1] = row[2];
-	row[2] = row[3];
-	row[3] = temp;
+	int n = row.size();
+	
+	for(int i=0; i<n-1;i++)
+		row[i] = row[i+1];
+
+	row[n-1] = temp;
 }
 void shiftRows_transform(vector< vector< uint8_t > > &state )
 {
@@ -41,7 +45,7 @@ void shiftRows_transform(vector< vector< uint8_t > > &state )
 
 void mixColumns_transform(vector< vector< uint8_t > > &state )
 {
-	for(int j = 0; j < 4; j++ )
+	for(int j = 0; j < state[0].size(); j++ )
 	{
 		state[0][j] = (2 * state[0][j]) ^ ( 3 * state[1][j] ) ^ ( 1 * state[2][j] ) ^ ( 1 * state[3][j] );
 		state[1][j] = (1 * state[0][j]) ^ ( 2 * state[1][j] ) ^ ( 3 * state[2][j] ) ^ ( 1 * state[3][j] );
